04cd157fdc29d6d55b4a66f9a02b77c2b71934e4
crazy0man <angelo.yin@gmail.com>
2014-09-23 11:05:09 +0800

    Reformat comments

--- Fast-Copy/Fast-Copy.ps1
+++ Fast-Copy/Fast-Copy.ps1
@@ -1,18 +1,59 @@
-# Fast-Copy large files
-# ref: http://blogs.technet.com/b/askperf/archive/2007/05/08/slow-large-file-copy-issues.aspx
-# Several copy method
-# robocopy
-# xcopy /j without cache
+<#
+.SYNOPSIS
+	Fast-Copy is to resolve big data copy on windows.
+.DESCRIPTION
+	We usually do Hyper-V VM data copy from one serve to another. But data
+is to T size. It is not good to use manual copy. It is not trackable and
+explorer is always hanging up. Also for copy mount of files in small size.
 
-# TODO
-# Requirements:
-# Copy from src list to one dest path
+BigData when data > 1G, we d better use it
 
-# TODO
-#Issue about log
-#not use start-process to execute cmd, just output to console
+Requirements:
+	1. Better performance
+	2. Copy log to track
+	3. Run in command line
 
-#BigData when data > 1G, we'd better use it
+Copy method:
+	1. robocopy
+	2. xcopy /j without cache
+
+.NOTES
+TODO:
+	1. Copy from src list to one dest path
+	2. not use start-process to execute cmd, just output to console
+
+Robocopy parameter using:
+	/E :: copy subdirectories, including Empty ones.
+	/Z :: copy files in restartable mode.
+	/ZB :: use restartable mode; if access denied use Backup mode.
+	/PF :: check run hours on a Per File (not per pass) basis.
+	/MT[:n] :: Do multi-threaded copies with n threads (default 8).
+		n must be at least 1 and not greater than 128.
+		This option is incompatible with the /IPG and /EFSRAW options.
+	Redirect output using /LOG option for better performance.
+	/R:n :: number of Retries on failed copies: default 1 million.
+	/V :: produce Verbose output, showing skipped files.
+	/FP :: include Full Pathname of files in the output.
+	/ETA :: show Estimated Time of Arrival of copied files.
+	/LOG:file :: output status to LOG file (overwrite existing log).
+	/TEE :: output to console window, as well as the log file.
+
+XCOPY parameter using:
+	/E	Copies directories and subdirectories, including empty ones.
+		Same as /S /E. May be used to modify /T.
+	/V	Verifies the size of each new file.
+	/F	Displays full source and destination file names while copying.
+	/Y	Suppresses prompting to confirm you want to overwrite an 
+		existing destination file.
+	/Z	Copies networked files in restartable mode.
+	/J	Copies using unbuffered I/O. Recommended for very large files.
+.LINK
+http://blogs.technet.com/b/askperf/archive/2007/05/08/slow-large-file-copy-issues.aspx
+
+.EXAMPLE
+
+.EXAMPLE
+#>
 
 param(
     [Parameter(Mandatory = $true, Position = 1)]
@@ -33,23 +74,6 @@ if (-not (Test-Path $DestPath)) {
 
 function Do-Robocopy ($src, $dest) {
     Write-Host "Start to do robocopy"
-    #retry 10times
-    #MT with 8 threads
-    #                 /E :: copy subdirectories, including Empty ones.
-    #                 /Z :: copy files in restartable mode.
-    #                /ZB :: use restartable mode; if access denied use Backup mode.
-    #                /PF :: check run hours on a Per File (not per pass) basis.
-    #            /MT[:n] :: Do multi-threaded copies with n threads (default 8).
-    #                       n must be at least 1 and not greater than 128.
-    #                       This option is incompatible with the /IPG and /EFSRAW options.
-    #                       Redirect output using /LOG option for better performance.
-    #               /R:n :: number of Retries on failed copies: default 1 million.
-    #                 /V :: produce Verbose output, showing skipped files.
-    #                /FP :: include Full Pathname of files in the output.
-    #               /ETA :: show Estimated Time of Arrival of copied files.
-    #          /LOG:file :: output status to LOG file (overwrite existing log).
-    #               /TEE :: output to console window, as well as the log file.
-    #/LOG:log.txt, need timestamp on log file name
     $args = "$SrcPath $DestPath /E /ZB /MT /COPYALL /R:10 /V /PF /FP /ETA /TEE /LOG:" + (Get-LogName)
     Start-Process "robocopy" $args -Wait
     Write-Host "End to do robocopy"
@@ -63,15 +87,6 @@ function Get-LogName
 }
 
 function Do-XCopy ($src, $dest) {
-    # XCOPY
-    #  /E           Copies directories and subdirectories, including empty ones.
-    #               Same as /S /E. May be used to modify /T.
-    #  /V           Verifies the size of each new file.
-    #  /F           Displays full source and destination file names while copying.
-    #  /Y           Suppresses prompting to confirm you want to overwrite an
-    #               existing destination file.
-    #  /Z           Copies networked files in restartable mode.
-    #  /J           Copies using unbuffered I/O. Recommended for very large files.
     Write-Host "Start XCopy from $src to $dest"
     $args = "$src $dest /E /V /F /Y /Z /J"
     Start-Process "XCopy" $args -Wait -RedirectStandardOutput (Get-LogName)
